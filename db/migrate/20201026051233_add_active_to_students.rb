9b32e083b36c00ac5d0e3a82b24295313997db75
Charles Butler <cdbutle@outlook.com>
2020-10-26 05:14:04 +0000

    Automatically backed up by Learn

--- db/migrate/20201026051233_add_active_to_students.rb
+++ db/migrate/20201026051233_add_active_to_students.rb
@@ -1,5 +1,5 @@
 class AddActiveToStudents < ActiveRecord::Migration[5.0]
-  def change
-    add_column :students, :active, :boolean :default => false
+    def change
+      add_column :students, :active, :boolean, :default => false
+    end
   end
-end
